107f94922735d19dd9f6860355700720c583758e
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-10-21 10:35:17 +0000

    Style path

--- picocards/pages/_app.js
+++ picocards/pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
-import '../styles/maker-card.css';
-import '../styles/mc-style.css';
+import 'styles/globals.css'
+import 'styles/maker-card.css';
+import 'styles/mc-style.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
