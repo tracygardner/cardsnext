f4146d41731c11b0e125faeb0b8f500809a8f048
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-10-21 11:15:07 +0000

    Update sw config

--- picocards/next.config.js
+++ picocards/next.config.js
@@ -10,7 +10,10 @@ const nextConfig = {
 module.exports = nextConfig
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  register: true,
+  scope: '/',
+  sw: 'sw.js'
 })
 
 module.exports = withPWA({
