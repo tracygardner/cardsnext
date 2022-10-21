1d3e2a29bba0fda15cbdfe2c6dd26310b482f066
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-10-21 16:41:07 +0000

    Logo

--- picocards/components/_cards.js
+++ picocards/components/_cards.js
@@ -17,7 +17,7 @@ export const CardHead = (props) => {
 export const Header = (props) => {
   return <header className="border-bottom secondary">
     <section className="wrap">
-      <img className="logo narrow" src="/images/RPF.png" alt="Raspberry Pi logo" />
+      <img className="logo narrow" src="images/RPF.png" alt="Raspberry Pi logo" />
       <h1>{props.heading}</h1>
       <p>{props.desc}</p>
     </section>
